8fc2fc8d9d620ca1e04c1a1e75cbdc383af1bba1
Brock Grinstead <grinstba@grinstba-5510.rose-hulman.edu>
2018-09-28 23:05:11 -0400

    Implemented routing

--- murphy-diesel/src/app/app.module.ts
+++ murphy-diesel/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactpageComponent } from './contactpage/contactpage.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent},
+  { path: 'contact', component: ContactpageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent,
+    ContactpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent, HeaderComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
